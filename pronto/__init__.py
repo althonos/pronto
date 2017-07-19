d97ece752f3bb91decec55df2902406c9189edb4
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-07-19 13:14:28 +0200

    Release v0.8.0

--- pronto/__init__.py
+++ pronto/__init__.py
@@ -8,7 +8,7 @@
 from __future__ import absolute_import
 
 __all__ = ["Ontology", "Term", "TermList", "Relationship"]
-__version__='0.7.4'
+__version__='0.8.0'
 __author__='Martin Larralde'
 __author_email__ = 'martin.larralde@ens-cachan.fr'
 

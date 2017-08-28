01402503b65f40d60a0b6885c848a5188125244e
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-08-28 10:05:23 +0200

    Move setup.py metadata to setup.cfg

--- pronto/__init__.py
+++ pronto/__init__.py
@@ -1,24 +1,21 @@
 # coding: utf-8
 """
-**pronto**: a Python frontend to ontologies
-===========================================
-
+a Python frontend to ontologies
 """
 
 from __future__ import absolute_import
 
-__all__ = ["Ontology", "Term", "TermList", "Relationship"]
 __version__='0.8.0'
 __author__='Martin Larralde'
 __author_email__ = 'martin.larralde@ens-cachan.fr'
-
-
-try:
-    from .ontology import Ontology
-    from .term import Term, TermList
-    from .relationship import Relationship
-except ImportError:
-    pass
+__all__ = [
+    "Ontology", "Term", "TermList", "Relationship", "Synonym", "SynonymType"
+]
 
 
 
+# try:
+from .ontology import Ontology
+from .term import Term, TermList
+from .relationship import Relationship
+from .synonym import Synonym, SynonymType

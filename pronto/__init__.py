e4580a598e7d930ad90f5480751804fc1fa89826
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-09-12 16:42:17 +0200

    Remove `pkg_resources` from the top-level package

--- pronto/__init__.py
+++ pronto/__init__.py
@@ -2,7 +2,12 @@ import pkg_resources
 
 __author__ = "Martin Larralde <martin.larralde@embl.de>"
 __license__ = "MIT"
-__version__ = pkg_resources.resource_string(__name__, "_version.txt").decode('utf-8').strip()
+__version__ = (
+    __import__('pkg_resources')
+    .resource_string(__name__, "_version.txt")
+    .decode('utf-8')
+    .strip()
+)
 
 from .ontology import Ontology  # noqa: F401
 from .term import Term  # noqa: F401

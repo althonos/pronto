3c8733d93835086d9c4e0743a1e78400064627c9
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-09-16 15:37:19 +0200

    Refactor common code between `Term` and `Relationship` and improve `Term._from_ast`

---
+++ pronto/entity.py
@@ -0,0 +1,217 @@
+
+import weakref
+import typing
+from typing import Callable, Optional, FrozenSet
+
+from .definition import Definition
+from .synonym import Synonym
+from .xref import Xref
+
+from .utils.repr import make_repr
+
+if typing.TYPE_CHECKING:
+    from .ontology import Ontology
+
+
+class EntityData():
+    pass
+
+
+class Entity():
+
+    _ontology: Callable[[], 'Ontology']
+    _data: Callable[[], EntityData]
+
+    __slots__ = ("__weakref__", "_ontology", "_data")
+
+    def __init__(self, ontology: 'Ontology', data):
+        self._ontology = weakref.ref(ontology)
+        self._data = weakref.ref(data)
+
+    # --- Magic Methods ------------------------------------------------------
+
+    def __eq__(self, other):
+        if not isinstance(other, Entity):
+            return False
+        return self.id == other.id
+
+    def __lt__(self, other):
+        if not isinstance(other, Entity):
+            return NotImplemented
+        return self.id < other.id
+
+    def __le__(self, other):
+        if not isinstance(other, Entity):
+            return NotImplemented
+        return self.id <= other.id
+
+    def __gt__(self, other):
+        if not isinstance(other, Entity):
+            return NotImplemented
+        return self.id > other.id
+
+    def __ge__(self, other):
+        if not isinstance(other, Entity):
+            return NotImplemented
+        return self.id >= other.id
+
+    def __hash__(self):
+        return hash((self.id))
+
+    def __repr__(self):
+        return make_repr(type(self).__name__, self.id, name=(self.name, None))
+
+    # --- Data descriptors ---------------------------------------------------
+
+    @property
+    def alternate_ids(self) -> FrozenSet[str]:
+        return frozenset(self._data().alternate_ids)
+
+    @alternate_ids.setter
+    def alternate_ids(self, ids: FrozenSet[str]):
+        if __debug__:
+            msg = "'alternate_ids' must be a set of str, not {}"
+            if not isinstance(ids, (set, frozenset)):
+                raise TypeError(msg.format(type(ids).__name__))
+            for x in (x for x in ids if isinstance(x, str)):
+                raise TypeError(msg.format(type(x).__name__))
+        self._data().alternate_ids = set(ids)
+
+    @property
+    def anonymous(self) -> bool:
+        return self._data().anonymous
+
+    @anonymous.setter
+    def anonymous(self, value: bool):
+        if __debug__:
+            if not isinstance(value, bool):
+                msg = "'anonymous' must be bool, not {}"
+                raise TypeError(msg.format(type(value).__name__))
+        self._data().anonymous = value
+
+    @property
+    def builtin(self) -> bool:
+        return self._data().builtin
+
+    @builtin.setter
+    def builtin(self, value: bool):
+        if __debug__:
+            if not isinstance(value, bool):
+                msg = "'builtin' must be bool, not {}"
+                raise TypeError(msg.format(type(value).__name__))
+        self._data().builtin = value
+
+    @property
+    def comment(self) -> Optional[str]:
+        return self._data().comment
+
+    @comment.setter
+    def comment(self, value: Optional[str]):
+        if __debug__:
+            if value is not None and not isinstance(value, str):
+                msg = "'comment' must be str or None, not {}"
+                raise TypeError(msg.format(type(value).__name__))
+        self._data().comment = value
+
+    @property
+    def definition(self) -> Optional[Definition]:
+        return self._data().definition
+
+    @definition.setter
+    def definition(self, definition: Optional[Definition]):
+        if __debug__:
+            if definition is not None and not isinstance(definition, Definition):
+                msg = "'definition' must be a Definition, not {}"
+                raise TypeError(msg.format(type(definition).__name__))
+        self._data().definition = definition
+
+    @property
+    def id(self):
+        return self._data().id
+
+    @id.setter
+    def id(self, value):
+        raise RuntimeError("cannot set `id` of entities directly")
+
+    @property
+    def name(self) -> Optional[str]:
+        return self._data().name
+
+    @name.setter
+    def name(self, value: Optional[str]):
+        if __debug__:
+            if value is not None and not isinstance(value, str):
+                msg = "'name' must be str or None, not {}"
+                raise TypeError(msg.format(type(value).__name__))
+        self._data().name = value
+
+    @property
+    def namespace(self) -> Optional[str]:
+        return self._data().namespace
+
+    @namespace.setter
+    def namespace(self, ns: Optional[str]):
+        if __debug__:
+            if ns is not None and not isinstance(ns, str):
+                msg = "'namespace' must be str or None, not {}"
+                raise TypeError(msg.format(type(ns).__name__))
+        self._data().namespace = ns
+
+    @property
+    def obsolete(self) -> bool:
+        return self._data().obsolete
+
+    @obsolete.setter
+    def obsolete(self, value: bool):
+        if __debug__:
+            if not isinstance(value, bool):
+                msg = "'obsolete' must be bool, not {}"
+                raise TypeError(msg.format(type(value).__name__))
+        self._data().obsolete = value
+
+    @property
+    def subsets(self) -> FrozenSet[str]:
+        return frozenset(self._data().subsets)
+
+    @subsets.setter
+    def subsets(self, subsets: FrozenSet[str]):
+        if __debug__:
+            msg = "'subsets' must be a set of str, not {}"
+            if not isinstance(subsets, (set, frozenset)):
+                raise TypeError(msg.format(type(subsets).__name__))
+            for x in (x for x in subsets if not isinstance(x, str)):
+                raise TypeError(msg.format(type(x).__name__))
+        for subset in subsets:
+            subsetdefs = self._ontology().metadata.subsetdefs
+            if not any(subset == subsetdef.id for subsetdef in subsetdefs):
+                raise ValueError(f"undeclared subset: {subset!r}")
+        self._data().subsets = set(subsets)
+
+    @property
+    def synonyms(self) -> FrozenSet[Synonym]:
+        ontology, termdata = self._ontology(), self._data()
+        return frozenset(Synonym(ontology, s) for s in termdata.synonyms)
+
+    @synonyms.setter
+    def synonyms(self, synonyms: FrozenSet[Synonym]):
+        if __debug__:
+            msg = "'synonyms' must be a set of Synonym, not {}"
+            if not isinstance(synonyms, (set, frozenset)):
+                raise TypeError(msg.format(type(synonyms).__name__))
+            for x in (x for x in synonyms if not isinstance(x, Synonym)):
+                raise TypeError(msg.format(type(x).__name__))
+        self._data().synonyms = set(synonyms)
+
+    @property
+    def xrefs(self) -> FrozenSet[Xref]:
+        return frozenset(self._data().xrefs)
+
+    @xrefs.setter
+    def xrefs(self, xrefs: FrozenSet[Xref]):
+        if __debug__:
+            msg = "'xrefs' must be a set of Xref, not {}"
+            if not isinstance(xrefs, (set, frozenset)):
+                raise TypeError(msg.format(type(xrefs).__name__))
+            for x in (x for x in synonyms if not isinstance(x, Xref)):
+                raise TypeError(msg.format(type(x).__name__))
+        self._data().xrefs = set(xrefs)
--- pronto/term.py
+++ pronto/term.py
@@ -1,10 +1,11 @@
 import datetime
 import typing
 import weakref
-from typing import Callable, Dict, List, Optional, Set, Tuple, Union
+from typing import Callable, Dict, List, Optional, Set, Tuple, Union, FrozenSet
 
 import fastobo
 
+from .entity import Entity
 from .definition import Definition
 from .xref import Xref
 from .synonym import Synonym, _SynonymData
@@ -29,7 +30,7 @@ class _TermData():  # noqa: R0902, R0903
     subsets: Set[str]
     namespace: Optional[str]
     xrefs: Set[Xref]
-    intersection_of: Set[Union[str, Tuple[Relationship, str]]]
+    intersection_of: Set[Union[str, Tuple[str, str]]]
     union_of: Set[str]
     disjoint_from: Set[str]
     relationships: Dict[str, Set[str]]
@@ -39,6 +40,7 @@ class _TermData():  # noqa: R0902, R0903
     builtin: bool
     created_by: Optional[str]
     creation_date: Optional[datetime.datetime]
+    equivalent_to: Set[str]
     annotations: Dict[str, List[str]]
 
     __slots__ = ("__weakref__",) + tuple(__annotations__)  # noqa: E0602
@@ -65,6 +67,7 @@ class _TermData():  # noqa: R0902, R0903
         builtin=False,
         created_by=None,
         creation_date=None,
+        equivalent_to=None,
         annotations=None,
     ):
         self.id = id
@@ -87,77 +90,76 @@ class _TermData():  # noqa: R0902, R0903
         self.builtin = builtin
         self.created_by = created_by
         self.creation_date = creation_date
+        self.equivalent_to = equivalent_to or set()
         self.annotations = annotations or dict()
 
 
-class Term(object):
-
-    _ontology: Callable[[], 'Ontology']
-    _termdata: Callable[[], _TermData]
-
-    __slots__  = ("__weakref__",) + tuple(__annotations__)   # noqa: E0602
+class Term(Entity):
 
     def __init__(self, ontology: 'Ontology', termdata: '_TermData'):
         self._ontology = weakref.ref(ontology)
-        self._termdata = weakref.ref(termdata)
+        self._data = weakref.ref(termdata)
 
     @classmethod
     def _from_ast(cls, frame: fastobo.term.TermFrame, ontology: 'Ontology'):
 
-        ontology._terms[str(frame.id)] = termdata = _TermData(str(frame.id))
-        term = cls(ontology, termdata)
+        term = ontology.create_term(str(frame.id))
+        termdata = term._data()
 
         union_of = set()
         intersection_of = set()
 
-        for clause in frame:
-            if clause.raw_tag() == "is_anonymous":
-                term.anonymous = clause.anonymous
-            elif clause.raw_tag() == "name":
-                term.name = clause.name
-            elif clause.raw_tag() == "alt_id":
-                term.alternate_ids.add(str(clause.alt_id))
-            elif clause.raw_tag() == "def":
-                term.definition = Definition._from_ast(clause)
-            elif clause.raw_tag() == "intersection_of":
-                pass # TODO in `fastobo-py`
-            elif clause.raw_tag() == "union_of":
-                union_of.add(str(clause.term))
-            elif clause.raw_tag() == "disjoint_from":
-                term.disjoint_from.add(str(clause.term))
-            elif clause.raw_tag() == "relationship":
-                pass # TODO in fastobo-py
-            elif clause.raw_tag() == "builtin":
-                term.builtin = clause.builtin
-            elif clause.raw_tag() == "comment":
-                term.comment = clause.comment
-            elif clause.raw_tag() == "consider":
-                pass # TODO in fastobo-py
-            elif clause.raw_tag() == "created_by":
-                pass # TODO in fastobo-py
-            elif clause.raw_tag() == "creation_date":
-                pass # TODO in fastobo-py
-            elif clause.raw_tag() == "equivalent_to":
-                term.equivalent_to.add(str(clause.term))
-            elif clause.raw_tag() == "is_a":
-                term.relationships.setdefault("is_a", set()).add(str(clause.term))
-            elif clause.raw_tag() == "is_obsolete":
-                term.obsolete = clause.obsolete
-            elif clause.raw_tag() == "namespace":
-                term.namespace = str(clause.namespace)
-            elif clause.raw_tag() == "property_value":
-                pass  # TODO in here
-            elif clause.raw_tag() == "replaced_by":
-                pass  # TODO in fastobo-py
-            elif clause.raw_tag() == "subset":
-                term.subsets.add(str(clause.subset))
-            elif clause.raw_tag() == "synonym":
-                termdata.synonyms.add(_SynonymData._from_ast(clause.synonym))
-            elif clause.raw_tag() == "xref":
-                term.xrefs.add(Xref._from_ast(clause.xref))
-            else:
-                raise ValueError(f"unexpected clause: {clause}")
+        def copy(src, dst=None, cb=None):
+            cb = cb or (lambda x: x)
+            dst = dst or src
+            return lambda c: setattr(term, dst, cb(getattr(c, src)))
 
+        def add(src, dst=None, cb=None):
+            cb = cb or (lambda x: x)
+            dst = dst or src
+            return lambda c: getattr(termdata, dst).add(cb(getattr(c, src)))
+
+        def todo():
+            return lambda c: print("todo", c)
+
+        _callbacks = {
+            fastobo.term.IsAnonymousClause: copy("anonymous"),
+            fastobo.term.NameClause: copy("name"),
+            fastobo.term.DefClause:
+                lambda c: setattr(term, "definition", Definition._from_ast(c)),
+            fastobo.term.AltIdClause: add("alt_id", "alternate_ids", cb=str),
+            fastobo.term.IntersectionOfClause: lambda c: (
+                intersection_of.add((str(c.typedef), str(c.term)))
+                if c.typedef is not None
+                else intersection_of.add(str(c.term))
+            ),
+            fastobo.term.UnionOfClause: lambda c: union_of.add(str(c.term)),
+            fastobo.term.RelationshipClause:
+                lambda c: termdata.relationships.setdefault(str(c.typedef), set()).add(str(c.term)),
+            fastobo.term.BuiltinClause: copy("builtin"),
+            fastobo.term.CommentClause: copy("comment"),
+            fastobo.term.ConsiderClause: add("term", "consider", cb=str),
+            fastobo.term.CreatedByClause: copy("creator", "created_by"),
+            fastobo.term.CreationDateClause: copy("date", "creation_date"),
+            fastobo.term.EquivalentToClause: add("equivalent_to", cb=str),
+            fastobo.term.IsAClause:
+                lambda c: termdata.relationships.setdefault("is_a", set()).add(str(c.term)),
+            fastobo.term.IsObsoleteClause: copy("obsolete"),
+            fastobo.term.NamespaceClause: copy("namespace", cb=str),
+            fastobo.term.SubsetClause: add("subset", "subsets", cb=str),
+            fastobo.term.SynonymClause:
+                lambda c: termdata.synonyms.add(_SynonymData._from_ast(c.synonym)),
+            fastobo.term.DisjointFromClause: add("term", "disjoint_from", cb=str),
+            fastobo.term.PropertyValueClause: todo(),
+            fastobo.term.ReplacedByClause: add("term", "replaced_by", cb=str),
+            fastobo.term.XrefClause: add("xref", "xrefs", cb=Xref._from_ast),
+        }
+
+        for clause in frame:
+            try:
+                _callbacks[type(clause)](clause)
+            except KeyError:
+                raise TypeError(f"unexpected type: {type(clause).__name__}")
         if len(union_of) == 1:
             raise ValueError("'union_of' cannot have a cardinality of 1")
         termdata.union_of = union_of
@@ -200,165 +202,29 @@ class Term(object):
 
         return frame
 
-    def __str__(self):
-        return str(self._to_ast())
-
-    def __repr__(self):
-        return make_repr("Term", self.id, name=(self.name, None))
-
-    # --- Data descriptors ---------------------------------------------------
-
-    @property
-    def alternate_ids(self) -> Set[str]:
-        return self._termdata().alternate_ids
-
-    @alternate_ids.setter
-    def alternate_ids(self, ids: Set[str]):
-        if __debug__:
-            if not isinstance(ids, set) or any(not isinstance(x, str) for x in ids):
-                msg = "'name' must be a set of str, not {}"
-                raise TypeError(msg.format(type(ids).__name__))
-        self._termdata().alternate_ids = ids
-
-    @property
-    def anonymous(self) -> bool:
-        return self._termdata().anonymous
-
-    @anonymous.setter
-    def anonymous(self, value: bool):
-        if __debug__:
-            if not isinstance(value, bool):
-                msg = "'anonymous' must be bool, not {}"
-                raise TypeError(msg.format(type(value).__name__))
-        self._termdata().anonymous = value
-
-    @property
-    def builtin(self) -> bool:
-        return self._termdata().builtin
-
-    @builtin.setter
-    def builtin(self, value: bool):
-        if __debug__:
-            if not isinstance(value, bool):
-                msg = "'builtin' must be bool, not {}"
-                raise TypeError(msg.format(type(value).__name__))
-        self._termdata().builtin = value
-
-    @property
-    def comment(self) -> Optional[str]:
-        return self._termdata().comment
-
-    @comment.setter
-    def comment(self, value: Optional[str]):
-        if __debug__:
-            if value is not None and not isinstance(value, str):
-                msg = "'comment' must be str or None, not {}"
-                raise TypeError(msg.format(type(value).__name__))
-        self._termdata().comment = value
-
-    @property
-    def definition(self) -> Optional[Definition]:
-        return self._termdata().definition
-
-    @definition.setter
-    def definition(self, definition: Optional[Definition]):
-        if __debug__:
-            if definition is not None and not isinstance(definition, Definition):
-                msg = "'definition' must be a Definition, not {}"
-                raise TypeError(msg.format(type(definition).__name__))
-        self._termdata().definition = definition
-
     @property
     def disjoint_from(self) -> Set['Term']:
-        ontology, termdata = self._ontology(), self._termdata()
-        return {Term(ontology, ontology._terms[id]) for id in termdata.disjoint_from}
-
-
-    @property
-    def id(self):
-        return self._termdata().id
-
-    # @id.setter
-    # def id(self, value):
-    #     raise RuntimeError("cannot set `id` of terms")
-    #     self._termdata().id = value
+        ontology, termdata = self._ontology(), self._data()
+        return {
+            Term(ontology, ontology.get_term(id))
+            for id in termdata.disjoint_from
+        }
 
     @property
-    def name(self) -> Optional[str]:
-        return self._termdata().name
-
-    @name.setter
-    def name(self, value: Optional[str]):
-        if __debug__:
-            if value is not None and not isinstance(value, str):
-                msg = "'name' must be str or None, not {}"
-                raise TypeError(msg.format(type(value).__name__))
-        self._termdata().name = value
-
-    @property
-    def namespace(self) -> Optional[str]:
-        return self._termdata().namespace
-
-    @namespace.setter
-    def namespace(self, ns: Optional[str]):
-        if __debug__:
-            if ns is not None and not isinstance(ns, str):
-                msg = "'namespace' must be str or None, not {}"
-                raise TypeError(msg.format(type(ns).__name__))
-        self._termdata().namespace = ns
-
-    @property
-    def obsolete(self) -> bool:
-        return self._termdata().obsolete
-
-    @obsolete.setter
-    def obsolete(self, value: bool):
-        if __debug__:
-            if not isinstance(value, bool):
-                msg = "'obsolete' must be bool, not {}"
-                raise TypeError(msg.format(type(value).__name__))
-        self._termdata().obsolete = value
-
-    @property
-    def relationships(self):
-        # FIXME
-        ontology, termdata = self._ontology(), self._termdata()
-        return {k: ontology[v] for k,v in termdata.relationships.items()}
-
-    @property
-    def subsets(self) -> Set[str]:
-        return self._termdata().subsets
-
-    @subsets.setter
-    def subsets(self, subsets: Set[str]):
-        if __debug__:
-            if not isinstance(subsets, set) or any(not isinstance(x, str) for x in subsets):
-                msg = "'subsets' must be a set of str, not {}"
-                raise TypeError(msg.format(type(subsets).__name__))
-        for subset in subsets:
-            subsetdefs = self._ontology().metadata.subsetdefs
-            if not any(subset == subsetdef.id for subsetdef in subsetdefs):
-                raise ValueError(f"undeclared subset: {subset!r}")
-        self._termdata().subsets = subsets
-
-    @property
-    def synonyms(self) -> Set[Synonym]:
-        ontology, termdata = self._ontology(), self._termdata()
-        return {Synonym(ontology, syndata) for syndata in termdata.synonyms}
-
-    @synonyms.setter
-    def synonyms(self, synonyms: Set[Synonym]):
-        if __debug__:
-            if not isinstance(synonyms, set) \
-            or any(not isinstance(x, Synonym) for x in synonyms):
-                msg = "'synonyms' must be a set of Synonym, not {}"
-                raise TypeError(msg.format(type(synonyms).__name__))
-        self._termdata().synonyms = synonyms
+    def relationships(self) -> Dict[Relationship, FrozenSet['Term']]:
+        ont, termdata = self._ontology(), self._data()
+        return {
+            Relationship(ont, ont.get_relationship(rel)._data()): frozenset(
+                Term(ont, ont.get_term(term)._data())
+                for term in terms
+            )
+            for rel,terms in termdata.relationships.items()
+        }
 
     @property
     def union_of(self) -> Set['Term']:
-        cls, termdata, ont = type(self), self._termdata(), self._ontology()
-        return set(cls(ont, id) for id in termdata.union_of)
+        cls, termdata, ont = type(self), self._data(), self._ontology()
+        return {cls(ont, id) for id in termdata.union_of}
 
     @union_of.setter
     def union_of(self, union_of: Set['Term']):
@@ -368,16 +234,9 @@ class Term(object):
                 raise TypeError(msg.format(type(union_of).__name__))
         if len(union_of) == 1:
             raise ValueError("'union_of' cannot have a cardinality of 1")
-        self._termdata().union_of = {term.id for term in union_of}
+        self._data().union_of = {term.id for term in union_of}
 
     @property
-    def xrefs(self) -> Set[Xref]:
-        return self._termdata().xrefs
-
-    @xrefs.setter
-    def xrefs(self, xrefs: Set[Xref]):
-        if __debug__:
-            if not isinstance(xrefs, set) or any(not isinstance(x, Xref) for x in xrefs):
-                msg = "'xrefs' must be a set of Xref, not {}"
-                raise TypeError(msg.format(type(xrefs).__name__))
-        self._termdata().xrefs = xrefs
+    def consider(self) -> FrozenSet['Term']:
+        cls, termdata, ont = type(self), self._data(), self._ontology()
+        return frozenset(cls(ont, id) for id in termdata.consider)

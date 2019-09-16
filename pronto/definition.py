cc2bc730a32d08fa29f338a91db2668be9addfe2
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-09-16 20:57:47 +0200

    Improve documentation of `pronto.Definition`

--- pronto/definition.py
+++ pronto/definition.py
@@ -9,7 +9,40 @@ from .utils.repr import make_repr
 
 
 class Definition(str):
-    """The definition of a term or a relationship, backed by some xrefs.
+    """The definition of a term or a relationship.
+
+    Definitions are human-readable descriptions of an entity in the ontology
+    graph, with some optional cross-references to support the definition.
+
+    Example:
+        Simply create a `Definition` instance by giving it a string::
+
+        >>> def1 = pronto.Definition('a structural anomaly')
+
+        Additional cross-references can be passed as arguments, or added later
+        to the ``xrefs`` attribute of the `Definition`:
+
+        >>> def2 = pronto.Definition('...', xrefs={pronto.Xref('MGI:Anna')})
+        >>> def2.xrefs.add(pronto.Xref('ORCID:0000-0002-3947-4444'))
+
+    Caution:
+        A `Definition` compare only based on its textual value, independently
+        of the `Xref` it may contains:
+
+        >>> def2 == pronto.Definition('...')
+        True
+
+    Note:
+        Some ontologies use the xrefs of a description to attribute the
+        authorship of that definition:
+
+        >>> mp = pronto.Ontology("http://purl.obolibrary.org/obo/mp.obo")
+        >>> sorted(mp['MP:3000003'].definition.xrefs)
+        [Xref('GOC:TermGenie'), Xref('MGI:Anna')]
+
+        The common usecase however is to refer to the source of a definition
+        using persistent identifiers like ISBN book numbers, or
+
     """
 
     xrefs: typing.Set[Xref]
@@ -21,17 +54,14 @@ class Definition(str):
         return cls(clause.definition, set(map(Xref._from_ast, clause.xrefs)))
 
     def _to_ast(self) -> fastobo.term.DefClause:
-        xrefs = [
-            Xref._to_ast(x)
-            for x in sorted(self.xrefs, key=attrgetter("id", "description"))
-        ]
+        xrefs = [Xref._to_ast(x) for x in sorted(self.xrefs)]
         return fastobo.term.DefClause(str(self), xrefs)
 
     def __new__(cls, text, xrefs=None):
         return super(Definition, cls).__new__(cls, text)
 
     def __init__(self, text, xrefs=None):
-        self.xrefs = xrefs or set()
+        self.xrefs = set(xrefs) if xrefs is not None else set()
 
     def __repr__(self):
         return make_repr("Definition", str(self), xrefs=(self.xrefs, set()))

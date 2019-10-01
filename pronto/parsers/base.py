fd4c62b157cfb4f5814e01640cd5d29837092cfc
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2019-10-01 14:27:39 +0200

    Improve local import detection in `BaseParser.process_imports`

--- pronto/parsers/base.py
+++ pronto/parsers/base.py
@@ -24,17 +24,24 @@ class BaseParser(abc.ABC):
         return NotImplemented
 
     def process_imports(self):
+
+        basepath = os.path.dirname(self.ont.path or "")
+
         if self.ont.import_depth != 0:
             for ref in self.ont.metadata.imports:
                 s = urllib.parse.urlparse(ref).scheme
                 if s in {"ftp", "http", "https"} or os.path.exists(ref):
                     url = ref
-                elif os.path.exists(f"{ref}.obo"):
-                    url = f"{ref}.obo"
-                elif os.path.exists(f"{ref}.json"):
-                    url = f"{ref}.json"
                 else:
-                    url = f"http://purl.obolibrary.org/obo/{ref}.obo"
+                    for ext in ["", ".obo", ".json", ".owl"]:
+                        if os.path.exists(os.path.join(basepath, f"{ref}{ext}")):
+                            url = os.path.join(basepath, f"{ref}{ext}")
+                            break
+                    else:
+                        if not os.path.splitext(ref)[1]:
+                            ref = f"{ref}.obo"
+                        url = f"http://purl.obolibrary.org/obo/{ref}"
+
                 self.ont.imports[ref] = type(self.ont)(
                     url,
                     max(self.ont.import_depth-1, 0),

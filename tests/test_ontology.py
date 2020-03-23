77e3a2e9bf72c52cc2faa8c7b7b9b112b7046d94
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-03-23 17:56:15 +0100

    Add tests for pickling/unpickling `Ontology` instances

--- tests/test_ontology.py
+++ tests/test_ontology.py
@@ -1,16 +1,16 @@
-import itertools
 import os
+import sys
+import pickle
 import unittest
 import warnings
 
 import pronto
-from pronto.term import Term, TermData, TermSet
 from pronto.logic.lineage import Lineage
 
 from .utils import DATADIR
 
 
-class TestTerm(unittest.TestCase):
+class TestOntology(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         warnings.simplefilter('error')
@@ -41,3 +41,64 @@ class TestTerm(unittest.TestCase):
 
         t2.relationships = {}
         self.assertEqual(ont._inheritance, {t1.id: Lineage(), t2.id: Lineage()})
+
+
+class TestPickling(object):
+    @classmethod
+    def setUpClass(cls):
+        warnings.simplefilter('error')
+        warnings.simplefilter('ignore', category=UnicodeWarning)
+        cls.file = open(os.path.join(DATADIR, "ms.obo"), "rb")
+        cls.ms = pronto.Ontology(cls.file)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.file.close()
+        warnings.simplefilter(warnings.defaultaction)
+
+    # ------------------------------------------------------------------------
+
+    def _test_memory_pickling(self, protocol):
+        ont = pronto.Ontology()
+        t1 = ont.create_term("TST:001")
+        t1.name = "first name"
+
+        pickled = pickle.dumps(ont, protocol=protocol)
+        unpickled = pickle.loads(pickled)
+
+        self.assertEqual(ont.keys(), unpickled.keys())
+        self.assertEqual(ont["TST:001"], unpickled["TST:001"])
+        self.assertEqual(ont["TST:001"].name, unpickled["TST:001"].name)
+
+    def test_memory_pickling_3(self):
+        self._test_memory_pickling(3)
+
+    def test_memory_pickling_4(self):
+        self._test_memory_pickling(4)
+
+    @unittest.skipIf(sys.version_info < (3, 8), "protocol 5 requires Python 3.8+")
+    def test_memory_pickling_5(self):
+        self._test_memory_pickling(5)
+
+    # ------------------------------------------------------------------------
+
+    def _test_file_pickling(self, protocol):
+        pickled = pickle.dumps(self.ms, protocol=protocol)
+        unpickled = pickle.loads(pickled)
+        self.assertEqual(self.ms.keys(), unpickled.keys())
+        for key in self.ms.keys():
+            term_ms, term_pickled = self.ms[key]._data(), unpickled[key]._data()
+            for attr in term_ms.__annotations__:
+                attr_ms = getattr(term_ms, attr)
+                attr_pickled = getattr(term_pickled, attr)
+                self.assertEqual(attr_ms, attr_pickled)
+
+    def test_file_pickling_3(self):
+        self._test_file_pickling(3)
+
+    def test_file_pickling_4(self):
+        self._test_file_pickling(4)
+
+    @unittest.skipIf(sys.version_info < (3, 8), "protocol 5 requires Python 3.8+")
+    def test_file_pickling_5(self):
+        self._test_file_pickling(5)

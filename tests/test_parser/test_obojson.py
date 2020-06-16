0cc11bdb3ad3022311442cc954bc882ced71d2a0
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-06-17 01:15:41 +0200

    Vendor `obographs` data for `tests.test_parser.test_obojon`

--- tests/test_parser/test_obojson.py
+++ tests/test_parser/test_obojson.py
@@ -1,25 +1,32 @@
-import io
-import os
 import unittest
 import warnings
-import xml.etree.ElementTree as etree
+from pathlib import Path
 
 import pronto
 
 
 class TestOboJsonExamples(unittest.TestCase):
 
+    def setUp(self):
+        warnings.simplefilter("error")
+
+    def tearDown(self):
+        warnings.simplefilter(warnings.defaultaction)
+
     @staticmethod
-    def get_url(name):
-        return f"https://github.com/geneontology/obographs/raw/master/examples/{name}.json"
+    def get_path(name):
+        folder = Path(__file__).parent.parent / "data" / "obographs"
+        return str(folder / f"{name}.json")
 
     def test_abox(self):
-        ont = pronto.Ontology(self.get_url("abox"))
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", pronto.warnings.NotImplementedWarning)
+            ont = pronto.Ontology(self.get_path("abox"))
         self.assertEqual(len(ont.terms()), 3) # Person (implicit), Male and Female
 
     @unittest.expectedFailure
     def test_basic(self):
-        ont = pronto.Ontology(self.get_url("basic"))
+        ont = pronto.Ontology(self.get_path("basic"))
         self.assertIn("test manus ontology", ont.metadata.remarks)
         self.assertIn("UBERON:0002101", ont)
         self.assertIn("UBERON:0002470", ont)
@@ -30,7 +37,7 @@ class TestOboJsonExamples(unittest.TestCase):
         self.assertIn(ont["UBERON:0002102"], ont["UBERON:0002398"].relationships[ont["part_of"]])
 
     def test_equiv_node_set(self):
-        ont = pronto.Ontology(self.get_url("equivNodeSetTest"))
+        ont = pronto.Ontology(self.get_path("equivNodeSetTest"))
         self.assertIn("DOID:0001816", ont)
         self.assertIn("NCIT:C3088", ont)
         self.assertIn("Orphanet:263413", ont)
@@ -40,7 +47,7 @@ class TestOboJsonExamples(unittest.TestCase):
         self.assertIn(ont["Orphanet:263413"], ont["DOID:0001816"].equivalent_to)
 
     def test_obsoletion_example(self):
-        ont = pronto.Ontology(self.get_url("obsoletion_example"))
+        ont = pronto.Ontology(self.get_path("obsoletion_example"))
         self.assertIn("X:1", ont)
         self.assertIn("X:2", ont)
         self.assertIn("Y:1", ont)
@@ -49,3 +56,6 @@ class TestOboJsonExamples(unittest.TestCase):
         self.assertIn(ont["X:1"], ont["X:2"].replaced_by)
         self.assertTrue(ont["Y:2"].obsolete)
         self.assertTrue(ont["Y:1"], ont["Y:2"].replaced_by)
+
+    def test_nucleus(self):
+        ont = pronto.Ontology(self.get_path("equivNodeSetTest"))

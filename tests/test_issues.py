cec4cda230814149463d2067d97f07c0ef3d44a3
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-06-17 11:46:50 +0200

    Add a small test to check inheritance iterators work for nested imports

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -26,7 +26,7 @@ class TestIssues(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         warnings.simplefilter(warnings.defaultaction)
-        
+
     def test_parser_consistency(self):
         """Assert several runs on the same file give the same output.
 
@@ -48,3 +48,20 @@ class TestIssues(unittest.TestCase):
         """
         ont = pronto.Ontology(os.path.join(utils.DATADIR, "owen-jones-gen.obo"))
         self.assertEqual(str(ont["ONT0:ROOT"]), "Term('ONT0:ROOT', name='°')")
+
+    def test_nested_imports(self):
+        """Check an ontology importing an ontology with imports can be opened.
+        """
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', UnicodeWarning)
+            path = os.path.join(utils.DATADIR, "imagingMS.obo")
+            ims = pronto.Ontology(path)
+
+        self.assertIn("MS:1000031", ims)
+        self.assertIn("UO:0000000", ims)
+
+        self.assertIn("MS:1000040", ims["UO:0000000"].subclasses().to_set().ids)
+        self.assertIn("UO:0000000", ims["MS:1000040"].superclasses().to_set().ids)
+
+        self.assertEqual(ims.imports["ms"].metadata.ontology, "ms")
+        self.assertEqual(ims.imports["ms"].imports["uo.obo"].metadata.ontology, "uo")
